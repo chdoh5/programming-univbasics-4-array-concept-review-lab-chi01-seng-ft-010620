5cdfbc8dbdacac79e1bc034b9a910a6b6eac5dba
Christine Doherty <christineadoherty@gmail.com>
2019-12-16 22:37:00 +0000

    Automatically backed up by Learn

--- answers.rb
+++ answers.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
-      return index
+     return index
     end
   end
-  nil 
+  nil
 end
 
 def find_max_value(array)
@@ -28,9 +28,10 @@ def find_min_value(array)
       end
     end
   end
-  return min
+  min
 end
 
 
+      
 
-  
+
